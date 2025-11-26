fb709c79b5d3a688e7421c7befca88a4c3fce41f
Yogesh Priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-11-26 10:34:52 +0530

    video call

--- src/sockets/chat/index.js
+++ src/sockets/chat/index.js
@@ -16,6 +16,24 @@ export const chatSocket = (io) => {
     readHandlers(io, socket);
     editHandlers(io, socket);
 
+    socket.on("call-user", (data) => {
+      io.to(data.userToCall).emit("incoming-call", {
+        from: socket.id,
+        offer: data.offer,
+      });
+    });
+
+    socket.on("answer-call", (data) => {
+      io.to(data.to).emit("call-accepted", data.answer);
+    });
+
+    socket.on("ice-candidate", (data) => {
+      io.to(data.to).emit("ice-candidate", {
+        from: socket.id,
+        candidate: data.candidate,
+      });
+    });
+
     socket.on("disconnect", () => {
       console.log("User disconnected");
     });
